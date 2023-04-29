62927d477374b5dce073f6c0550f0bd44578eaad
imdancho <daniyar.0586@gmail.com>
2023-04-30 03:42:57 +0600

    Assignment 2

---
+++ src/MyArrayListQueue.java
@@ -0,0 +1,10 @@
+import java.util.EmptyStackException;
+import java.util.ArrayList;
+
+public class MyArrayListQueue {
+    static ArrayList<Object> stack = new ArrayList<Object>();
+
+    public MyArrayListQueue() {
+    }
+
+}
