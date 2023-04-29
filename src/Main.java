88b600833df54e605c58ee20c1b8d687556359dd
imdancho <daniyar.0586@gmail.com>
2023-04-30 03:07:04 +0600

    Assignment 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        MyArrayListStack.push(95);
+        MyArrayListStack.push(95);
+        MyArrayListStack.push(95);
+        MyArrayListStack.push(95);
+        System.out.println(MyArrayListStack.sizee());
 
+        MyArrayListStack.pop();
+        MyArrayListStack.pop();
 
-
-
-
-
-
-
-
-
+        System.out.println(MyArrayListStack.sizee());
 
 
 
--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -2,21 +2,21 @@ import java.util.EmptyStackException;
 import java.util.ArrayList;
 
 public class MyArrayListStack {
-    ArrayList<Object> stack = new ArrayList<Object>();
+    static ArrayList<Object> stack = new ArrayList<Object>();
 
     public MyArrayListStack() {
     }
 
-    public void push(Object o) {
+    public static void push(Object o) {
         stack.add(o);
     }
-    
-    public void pop(Object o) {
+
+    public static void pop() {
         int size = stack.size();
         stack.remove(size);
     }
 
-    public int sizee(Object o) {
+    public static int sizee() {
         return stack.size();
     }
 
