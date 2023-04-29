667481719fdc84e04bc8ab7ac658aff08aef79e7
imdancho <daniyar.0586@gmail.com>
2023-04-30 02:35:45 +0600

    Assignment 2

---
+++ src/MyArrayListStack.java
@@ -0,0 +1,13 @@
+import java.util.EmptyStackException;
+import java.util.ArrayList;
+
+public class MyArrayListStack {
+    ArrayList<Integer> stack = new ArrayList<Integer>();
+
+    public MyArrayListStack() {
+    }
+
+
+
+
+}
